b4e6056d884c241f9a7b7b56750c992a61b40668
dev_h2gremm <fahadalisarwar@gmail.com>
2022-06-13 14:26:05 +0200

    added second example

---
+++ Code/01.01_First_cplus_program/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main(){
+    auto result = (10 <=> 20) > 0;
+    std::cout << result << std::endl;
+    std::cout << result << std::endl;
+    return 0;
+    // return 0 is used to indicated to the OS that the operation was  successful
+    
+}
