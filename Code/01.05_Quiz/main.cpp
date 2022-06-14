408f9dc6f06d3edf66f53d33a1337cae95de114e
dev_h2gremm <fahadalisarwar@gmail.com>
2022-06-14 10:21:32 +0200

    added second section

---
+++ Code/01.05_Quiz/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main(){
+    std::cout << "Where do you live?" << std::endl;
+    std::string country;
+
+    std::cin >> country;
+
+    std::cout << "I have heard greate things about "<< country << ". I'd like to go sometime."<< std::endl;
+}
